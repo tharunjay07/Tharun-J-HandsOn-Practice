beaae22b61fd30770ccfb673821090a5a1997219
Tharun J <tharunjay2244@gmail.com>
2025-06-29 18:30:11 +0530

    Create BankAccount.java

---
+++ Week2/JUnit Basic Testing/Exercise4/BankAccount.java
@@ -0,0 +1,29 @@
+package com.example.JUnitDemo;
+
+public class BankAccount {
+
+    private double balance;
+    public BankAccount(double initialAmount) {
+        this.balance = initialAmount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Invalid withdraw amount.");
+        }
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+}
