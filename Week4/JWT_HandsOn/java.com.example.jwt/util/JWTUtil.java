6f2b171ce23ccc6f842b1614a7b493cb70cef471
Tharun J <tharunjay2244@gmail.com>
2025-07-13 20:33:34 +0530

    Create JWTUtil.java

---
+++ Week4/JWT_HandsOn/java.com.example.jwt/util/JWTUtil.java
@@ -0,0 +1,22 @@
+package com.example.jwt.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String secret = "secret123";
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 20 * 60 * 1000)) // 20 min
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
