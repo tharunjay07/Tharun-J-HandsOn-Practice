799bd9b89b7303560ccc19a654cb0da11db24dc4
Tharun J <tharunjay2244@gmail.com>
2025-08-07 10:44:22 +0530

    Create App.js

---
+++ Week7/React_HandsOn_12/ticketbookingapp/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Greeting from './components/Greeting';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogoutClick = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div style={{textAlign:"center"}}>
+      <Greeting isLoggedIn={isLoggedIn} />
+      {isLoggedIn ? (
+        <LogoutButton onClick={handleLogoutClick} />
+      ) : (
+        <LoginButton onClick={handleLoginClick} />
+      )}
+    </div>
+  );
+}
+
+export default App;
